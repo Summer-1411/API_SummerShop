fe7539c2edb7b658d906fa2f850d55db06cbd8f0
summer <levantung14112002@gmail.com>
2023-04-24 08:08:09 +0700

    update api user

--- routes/user.js
+++ routes/user.js
@@ -1,45 +1,67 @@
 const express = require('express')
 const router = express.Router()
 
-const {verifyToken,
+const { verifyToken,
     verifyTokenAndAuthorization,
-    verifyTokenAndAdmin} = require('../middleware/verifyToken')
+    verifyTokenAndAdmin } = require('../middleware/verifyToken')
 
 
 const pool = require('../common/connectDB')
 
-//UPDATE USER
-
-router.get("/alluser/", verifyTokenAndAdmin, async(req, res) =>{
-    const qid = req.query.id
+router.get("/page", async(req, res) => {
     try {
-        if(qid){
-            const [users] = await pool.query(`SELECT * FROM user WHERE id = ?`, [qid]);
-            return res.status(200).json({success: true, users})
-        }
-        const [users] = await pool.query(`SELECT * FROM user`);
-        return res.status(200).json({success: true, users})
+        let [page] = await pool.execute(`SELECT CEIL(COUNT(*) / 5) AS numPages FROM user WHERE deleted = ?`, [0])
+        return res.status(200).json(page)
     } catch (error) {
-        console.log(error);
         return res.status(500).json({success: false, message: "Internal server error !"})
     }
 })
 
-
-router.put("update/:id", verifyTokenAndAuthorization, async (req, res) => {
-    const {username, avatar , birthday} = req.body
-    if(!username || !avatar || !birthday){
-		return res
-		.status(400)
-		.json({ success: false, message: 'Missing parameters !' })
-	}
+router.get("/alluser/", verifyTokenAndAdmin, async (req, res) => {
+    const qid = req.query.id
+    const qpage = req.query.page
     try {
-        await pool.execute('UPDATE user SET username = ?, avatar = ?, birthday = ? WHERE id = ?', 
-            [username, avatar, birthday, req.params.id])
-        return res.status(200).json({success: true, message: "Cập nhật thông tin thành công"})
+        if (qid) {
+            const [users] = await pool.query(`SELECT * FROM user WHERE id = ?`, [qid]);
+            // const [countSuccess] = await pool.query(`SELECT COUNT(*) as number FROM orders WHERE id_user = ? AND status = ? GROUP BY id_user`, [qid, 2]);
+            // const [countCancel] = await pool.query(`SELECT COUNT(*) as number FROM orders WHERE id_user = ? AND status = ? GROUP BY id_user`, [qid, -1]);
+            return res.status(200).json({ success: true, users})
+        }else if(qpage){
+            let page = Number(qpage)
+            let limit = 5;
+            let offset = (page-1)*5;
+            let [users] = await pool.execute(`SELECT * FROM user WHERE deleted = ? AND isAdmin = ? ORDER BY createAt DESC LIMIT ${limit} OFFSET ${offset}`, [0, 0]);
+            return res.status(200).json({success: true, users})
+        }
+
+        const [users] = await pool.query(`SELECT * FROM user WHERE deleted = ? AND isAdmin = ? ORDER BY createAt DESC`, [0, 0]);
+        return res.status(200).json({ success: true, users })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Có lỗi xảy ra trong quá trình xử lý !" })
+    }
+})
+
+
+router.put("/update/:id", verifyTokenAndAuthorization, async (req, res) => {
+    const { username, avatar, gender, birthday } = req.body
+
+    const values = [];
+    if (username) values.push(`username='${username}'`);
+    if (avatar) values.push(`avatar='${avatar}'`);
+    if (gender) values.push(`gender='${gender}'`);
+    if (birthday) values.push(`birthday='${birthday}'`);
+
+    try {
+        await pool.execute(
+            `UPDATE user SET ${values.join(", ")} WHERE id = ?`,
+            [req.params.id]
+        );
+        console.log(req.body);
+        return res.status(200).json({ success: true, message: "Cập nhật thông tin thành công" })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: "Có lỗi xảy ra trong quá trình xử lý !" })
     }
 })
 
@@ -48,19 +70,19 @@ router.put("update/:id", verifyTokenAndAuthorization, async (req, res) => {
 //DELETE
 
 router.put("/delete/:id", verifyTokenAndAdmin, async (req, res) => {
-    const {deleted} = req.body
-    if(!deleted){
-		return res
-		.status(400)
-		.json({ success: false, message: 'Missing parameters !' })
-	}
+    const { deleted } = req.body
+    if (!deleted) {
+        return res
+            .status(400)
+            .json({ success: false, message: 'Missing parameters !' })
+    }
     try {
-        await pool.execute('UPDATE user SET deleted = ? WHERE id = ?', 
+        await pool.execute('UPDATE user SET deleted = ? WHERE id = ?',
             [deleted, req.params.id])
-        return res.status(200).json({success: true, message: "Xoá user thành công"})
+        return res.status(200).json({ success: true, message: "Xoá user thành công" })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Có lỗi xảy ra trong quá trình xử lý !" })
     }
 })
 
