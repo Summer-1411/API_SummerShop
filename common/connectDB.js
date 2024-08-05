8c7a5ff26e20f065ee56415048e174f16c4cdfee
summer <levantung14112002@gmail.com>
2024-08-05 21:52:23 +0700

    add connect db host

--- common/connectDB.js
+++ common/connectDB.js
@@ -1,31 +1,16 @@
-// const mysql = require("mysql2/promise")
-
-// const connection =  await mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'db'
-// })
-
-
-// console.log('Connected!');
-
-// module.exports = connection
-
 const mysql = require('mysql2/promise');
 
-// async function connectToDatabase() {
-//     const connection = await mysql.createPool({
-        
-//     });
-//     console.log("Connect DB");
-//     return connection;
-// }
+
+
+// host: "sql.freedb.tech",
+// user: "freedb_tunglv27",
+// password: "FgF!kE5TxPPzhW!",
+// database: "freedb_my-shop"
 const pool = mysql.createPool({
     host: "sql.freedb.tech",
     user: "freedb_tunglv27",
-    password: "FgF!kE5TxPPzhW!",
-    database: "freedb_my-shop"
+    password: "7cd9N!&Ta3?U@vN",
+    database: "freedb_db_shop_123"
 })
 
 pool.getConnection((err, conn) => {
