b20e339cd0b89f11a63152beb2d7585a2a265eec
summer <levantung14112002@gmail.com>
2024-11-01 22:09:58 +0700

    fix select DISTINCT

--- routes/filter.js
+++ routes/filter.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const {verifyToken,
+const { verifyToken,
     verifyTokenAndAuthorization,
-    verifyTokenAndAdmin} = require('../middleware/verifyToken')
+    verifyTokenAndAdmin } = require('../middleware/verifyToken')
 
 
 const pool = require('../common/connectDB')
@@ -13,20 +13,20 @@ const pool = require('../common/connectDB')
 router.get('/find/:id', async (req, res) => {
     try {
         const [filter] = await pool.execute(`SELECT * FROM filter WHERE id_pro=? AND deleted=?`, [req.params.id, 0])
-        return res.status(200).json({success: true, filter})
+        return res.status(200).json({ success: true, filter })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 //GET ALL COLOR - 1 PRODUCT by id_pro
 router.get('/color/:id', async (req, res) => {
     try {
-        const [colors] = await pool.execute(`SELECT color FROM filter WHERE id_pro=? AND deleted=? GROUP BY color`, [req.params.id, 0])
-        return res.status(200).json({success: true, colors})
+        const [colors] = await pool.execute(`SELECT DISTINCT color FROM filter WHERE id_pro=? AND deleted=?`, [req.params.id, 0])
+        return res.status(200).json({ success: true, colors })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 
@@ -34,11 +34,11 @@ router.get('/color/:id', async (req, res) => {
 //GET ALL SIZE - 1 PRODUCT by id_pro
 router.get('/size/:id', async (req, res) => {
     try {
-        const [sizes] = await pool.execute(`SELECT size FROM filter WHERE id_pro=? AND deleted=? GROUP BY size`, [req.params.id, 0])
-        return res.status(200).json({success: true, sizes})
+        const [sizes] = await pool.execute(`SELECT DISTINCT size FROM filter WHERE id_pro=? AND deleted=?`, [req.params.id, 0])
+        return res.status(200).json({ success: true, sizes })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 
@@ -47,11 +47,11 @@ router.get('/searchsize', async (req, res) => {
     const qcolor = req.query.color
     const qid_pro = req.query.idpro
     try {
-        const [sizes] = await pool.execute(`SELECT id, size FROM filter WHERE id_pro=? AND color=? AND deleted=? GROUP BY size`, [qid_pro, qcolor, 0])
-        return res.status(200).json({success: true, sizes})
+        const [sizes] = await pool.execute(`SELECT DISTINCT id, size FROM filter WHERE id_pro=? AND color=? AND deleted=?`, [qid_pro, qcolor, 0])
+        return res.status(200).json({ success: true, sizes })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 
@@ -60,24 +60,24 @@ router.get('/searchcolor', async (req, res) => {
     const qsize = req.query.size
     const qid_pro = req.query.idpro
     try {
-        const [colors] = await pool.execute(`SELECT id, color, img FROM filter WHERE id_pro=? AND size=? AND deleted=? GROUP BY color`, [qid_pro, qsize, 0])
-        return res.status(200).json({success: true, colors})
+        const [colors] = await pool.execute(`SELECT DISTINCT id, color, img FROM filter WHERE id_pro=? AND size=? AND deleted=?`, [qid_pro, qsize, 0])
+        return res.status(200).json({ success: true, colors })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 
 //Get img by color
-router.get("/img", async (req, res)=> {
+router.get("/img", async (req, res) => {
     const qcolor = req.query.color
     const qid_pro = req.query.idpro
     try {
         const [img] = await pool.execute(`SELECT img FROM filter WHERE id_pro=? AND deleted=? AND color=?`, [qid_pro, qcolor, 0])
-        return res.status(200).json({image: img[0]})
+        return res.status(200).json({ image: img[0] })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 
@@ -89,28 +89,28 @@ router.get('/details', async (req, res) => {
     const qid_pro = req.query.idpro
     try {
         const [detail] = await pool.execute(`SELECT * FROM filter WHERE id_pro=? AND size=? AND color=? AND deleted=?`, [qid_pro, qsize, qcolor, 0])
-        return res.status(200).json({success: true, detail: detail[0]})
+        return res.status(200).json({ success: true, detail: detail[0] })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 
 //Thêm bảng phân loại
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
-    const {id_pro, size, color, quantity, price, img} = req.body
+    const { id_pro, size, color, quantity, price, img } = req.body
     try {
-        const [result] = await pool.query('INSERT INTO filter (id_pro, size, color, quantity, price, img) VALUES (?, ?, ?, ?, ?, ?)', 
-        [id_pro, size, color, Number(quantity), Number(price), img]);
-        return res.status(200).json({success: true, message: "Thêm mới thông tin sản phẩm thành công", id: result.insertId})
+        const [result] = await pool.query('INSERT INTO filter (id_pro, size, color, quantity, price, img) VALUES (?, ?, ?, ?, ?, ?)',
+            [id_pro, size, color, Number(quantity), Number(price), img]);
+        return res.status(200).json({ success: true, message: "Thêm mới thông tin sản phẩm thành công", id: result.insertId })
     } catch (error) {
         console.log("error lỗi");
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 
 router.put("/update/:id", verifyTokenAndAdmin, async (req, res) => {
-    const {size, color, quantity, price, img} = req.body
+    const { size, color, quantity, price, img } = req.body
 
     const values = [];
     if (size) values.push(`size='${size}'`);
@@ -128,24 +128,24 @@ router.put("/update/:id", verifyTokenAndAdmin, async (req, res) => {
     //     return res.status(200).json({ success: true, message: "Cập nhật thông tin thành công" })
 
     try {
-        const [result] = await pool.execute(`UPDATE filter SET ${values.join(", ")} WHERE id=?`, 
-        [req.params.id]);
-        return res.status(200).json({success: true, message: "Cập nhật thông tin sản phẩm thành công"})
+        const [result] = await pool.execute(`UPDATE filter SET ${values.join(", ")} WHERE id=?`,
+            [req.params.id]);
+        return res.status(200).json({ success: true, message: "Cập nhật thông tin sản phẩm thành công" })
     } catch (error) {
         console.log("error lỗi");
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 
 router.put("/delete/:id", verifyTokenAndAdmin, async (req, res) => {
     console.log(req.params.id);
     try {
-        const [result] = await pool.execute('UPDATE filter SET deleted=? WHERE id=?', 
-        [1, req.params.id]);
-        return res.status(200).json({success: true, message: "Xoá thông tin sản phẩm thành công"})
+        const [result] = await pool.execute('UPDATE filter SET deleted=? WHERE id=?',
+            [1, req.params.id]);
+        return res.status(200).json({ success: true, message: "Xoá thông tin sản phẩm thành công" })
     } catch (error) {
         console.log("error lỗi", error);
-        return res.status(500).json({success: false, message: "Internal server error !"})
+        return res.status(500).json({ success: false, message: "Internal server error !" })
     }
 })
 module.exports = router
