75bd95e178e4efebb520c5718c9fac6b06553a53
summer <levantung14112002@gmail.com>
2024-12-19 23:22:43 +0700

    deploy backend

--- common/connectDB.js
+++ common/connectDB.js
@@ -6,12 +6,20 @@ const mysql = require('mysql2/promise');
 // user: "freedb_tunglv27",
 // password: "FgF!kE5TxPPzhW!",
 // database: "freedb_my-shop"
-const pool = mysql.createPool({
+
+const configDatabaseLocal = {
     host: "localhost",
     user: "root",
     password: "",
     database: "db_shop"
-})
+}
+const configDatabaseProduct = {
+    host: "sql.freedb.tech",
+    user: "freedb_freedb_summer_shop",
+    password: "237Q5zUxp?*B7%q",
+    database: "freedb_summer_shop-shop"
+}
+const pool = mysql.createPool(configDatabaseProduct)
 
 pool.getConnection((err, conn) => {
     if (err) {
